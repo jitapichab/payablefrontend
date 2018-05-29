b119b3648bde32032621fa2454795e89d0e521ca
REPOS <academico.rita@correo.udistrital.edu.co>
2018-05-29 18:57:35 -0500

    new contract payable indexed

---
+++ contracts/payable_indexed.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.0;
+
+contract PagarContrato {
+
+ 
+    constructor()  public payable { }
+    
+    event QuienPago(address indexed  pagador, string  fecha, uint cantidad);
+    event QuienCobro( address indexed cobrador, string  fecha, uint  cantidad);
+    function PagarMas(string fecha) public payable  {
+        emit QuienPago(msg.sender,fecha, msg.value);
+    }
+  
+    
+    function Cobrar(uint amount, string fecha) public  {
+            msg.sender.transfer(amount);
+            emit QuienCobro(msg.sender, fecha, amount);
+       }
+    
+    function AcumuladoContrato() view public returns (uint) {
+        return address(this).balance;
+    }
+}
