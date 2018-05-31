c84d6af44489821660e51fdaf67a319e69f069a8
REPOS <academico.rita@correo.udistrital.edu.co>
2018-05-31 09:57:20 -0500

    add new num.sol

---
+++ contracts/nums.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.6;
+contract MyContract {
+
+    uint num;
+
+    
+ 
+
+    event NumberSetEvent(address indexed caller, bytes32 indexed oldNum, bytes32 indexed newNum);
+
+    function getNum() returns (uint n) {
+        return num;
+    }
+
+    function setNum(uint n) {
+        uint old=num;
+        num=n;
+        NumberSetEvent(msg.sender,bytes32(old), bytes32(num));
+    }
+
+    function MyContract(uint x) { 
+        num=x;
+        
+    }
+
+   
+}
